81ea0732a7468cb370ddcd22f714e811a7b3f5d7
Sudhanshu-Singh-K <sudhanshu.2428cseai@kiet.edu>
2026-04-23 00:27:24 +0530

    Updated frontend build and API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://backend-v3nj.onrender.com/api",
 });
 
 // Attach token automatically
